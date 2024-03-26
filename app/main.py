9631610719576d10424256afae689c0987ee1694
Zarin Hoque <anan@zarins-air.home>
2024-03-26 17:06:35 -0400

    commented out import models from main.py file

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-import models
+# import models
 from database import engine
 from routers import post, user, auth, vote
 from config import settings
