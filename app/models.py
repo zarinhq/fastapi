733aa22a968051af4274940d200277f955c46e7a
Zarin Hoque <anan@zarins-air.home>
2024-03-26 17:26:34 -0400

    changed models.py file

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.sql.expression import text
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql.sqltypes import TIMESTAMP
-from database import Base
+from app.database import Base
 
 class Post(Base):
     __tablename__ = "posts"
