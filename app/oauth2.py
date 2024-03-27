1bfd5f70cf7c05b4bb2177eb8b2727a01b79596c
Zarin Hoque <anan@zarins-air.home>
2024-03-26 20:16:26 -0400

    edited ouath2.py file

--- app/oauth2.py
+++ app/oauth2.py
@@ -34,20 +34,20 @@ def verify_access_token(token: str, credentials_exception):
 
         if id is None:
             raise credentials_exception
-        token_data = schemas.TokenData(id = id)
+        token_data = app.schemas.TokenData(id = id)
     except JWTError:
         raise credentials_exception
     
     return token_data
     
-def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(database.get_db)):
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(app.database.get_db)):
     credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, 
                                           detail=f"Could not validate credentials", headers={"WWW-Authenticate": "Bearer"})
     
 
     token = verify_access_token(token, credentials_exception)
 
-    user = db.query(models.User).filter(models.User.id ==token.id).first()
+    user = db.query(app.models.User).filter(app.models.User.id ==token.id).first()
 
     return user
 
