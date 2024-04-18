a56ecf9c5625525c758b100cda36f0fd4de4c173
Zarin Hoque <anan@zarins-air.home>
2024-04-18 16:19:47 -0400

    adding first github actions

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -10,10 +10,10 @@ def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session =
     user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
 
     if not user:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail = f"Invalid credentials")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail = f"Invalid Credentials")
     
     if not utils.verify(user_credentials.password, user.password):
-        raise HTTPException(status_code = status.HTTP_403_FORBIDDEN, detail = f"Invalid credentials")
+        raise HTTPException(status_code = status.HTTP_403_FORBIDDEN, detail = f"Invalid Credentials")
     
     ##create token 
     ##return token
