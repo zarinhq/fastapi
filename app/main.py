4492f3e6ff8c58fc65a7d5433300b43409cebbe9
Zarin Hoque <anan@zarins-air.home>
2024-04-19 00:38:30 -0400

    push out to ubuntu

--- app/main.py
+++ app/main.py
@@ -27,7 +27,7 @@ app.include_router(vote.router)
 
 @app.get("/")
 def root():
-    return {"message": "Hello World successfully deployed from CI/CD pipeline"}
+    return {"message": "Hello World pushing out to ubuntu"}
 
   
 # def find_post(id):
